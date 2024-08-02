4f47ad906665475cf72036b51d4508e2674480f1
ryuguimago <janos.kimmig@gmail.com>
2024-08-02 12:02:42 +0200

    v1

--- roll.py
+++ roll.py
@@ -2,14 +2,14 @@
 #prompt user input / dont forget to convert to int
 # catch errors 
 x = 0
+anzahl = 0
 while x == 0:
     try:
         anzahl = int(input("How Many Dice Do You Want To Roll? [1-6]\n"))
-        x = 1
     except ValueError:
+        print("You're only allowed to use whole numbers")
+    if anzahl not in (1,2,3,4,5,6):
         print("please enter a number between 1 and 6")
-if anzahl in (1,2,3,4,5,6):
-    print("lets go")
-else:
-    print("please enter a number between 1 and 6")
-
+    else:
+        print("lets go")
+        x = 1
