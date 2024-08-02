71d60bf11c2ef21b5dc37bb0aa3140223dbc68e8
ryuguimago <janos.kimmig@gmail.com>
2024-08-02 11:52:50 +0200

    better but not there yet

--- roll.py
+++ roll.py
@@ -1,11 +1,15 @@
 #THIS IS A DICE ROLLING PROGRAM.
 #prompt user input / dont forget to convert to int
-try:
-    anzahl = int(input("How Many Dice Do You Want To Roll? [1-6]\n"))
-except ValueError:
-    print("please enter a number between 1 and 6")
-    
+# catch errors 
+x = 0
+while x == 0:
+    try:
+        anzahl = int(input("How Many Dice Do You Want To Roll? [1-6]\n"))
+        x = 1
+    except ValueError:
+        print("please enter a number between 1 and 6")
 if anzahl in (1,2,3,4,5,6):
     print("lets go")
 else:
     print("please enter a number between 1 and 6")
+
