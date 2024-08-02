690bcbaf93e8b4d81c531625141a33f27e6b7d77
ryuguimago <janos.kimmig@gmail.com>
2024-08-02 13:05:49 +0200

    almost there

--- roll.py
+++ roll.py
@@ -1,9 +1,47 @@
 #THIS IS A DICE ROLLING PROGRAM.
 #
 import random
+dice_art = {
+    1: """
+ _____
+|     |
+|  o  |
+|_____|
+""",
+    2: """
+ _____
+|o    |
+|     |
+|____o|
+""",
+    3: """
+ _____
+|o    |
+|  o  |
+|____o|
+""",
+    4: """
+ _____
+|o   o|
+|     |
+|o___o|
+""",
+    5: """
+ _____
+|o   o|
+|  o  |
+|o___o|
+""",
+    6: """
+ _____
+|o   o|
+|o   o|
+|o___o|
+"""
+}
 
-#roll function
-def rolling():
+#roll_dice function
+def roll_dice():
     result = random.randint(1,6)
     return result
 
@@ -18,12 +56,12 @@ while x == 0:                                                             #catch
     if anzahl not in (1,2,3,4,5,6):
         print("please enter a number between 1 and 6")
     else:
-        print("lets go")
         x = 1
 # the actual rolling of the dice
 values = []
 for i in range(anzahl):
-   value = rolling()
-   values.append(value)
-print(values)
+   pips = roll_dice()
+   values.append(pips)     #saves the result in a list
+for value in values:
+    print(dice_art[value])
     
