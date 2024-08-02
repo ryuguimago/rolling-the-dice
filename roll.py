c5d512a18e8a0c557b377287abe4346841314ee5
ryuguimago <janos.kimmig@gmail.com>
2024-08-02 12:29:37 +0200

    ...

--- roll.py
+++ roll.py
@@ -1,11 +1,18 @@
 #THIS IS A DICE ROLLING PROGRAM.
-#prompt user input / dont forget to convert to int
-# catch errors 
+#
+import random
+
+#roll function
+def rolling():
+    dice = random.randint(1,6)
+    print(dice)             #for testing purposes 
+
+#TUI
 x = 0
 anzahl = 0
-while x == 0:
+while x == 0:                                                             #catch errors
     try:
-        anzahl = int(input("How Many Dice Do You Want To Roll? [1-6]\n"))
+        anzahl = int(input("How Many Dice Do You Want To Roll? [1-6]")) #prompt user input / convert to int
     except ValueError:
         print("You're only allowed to use whole numbers")
     if anzahl not in (1,2,3,4,5,6):
@@ -13,3 +20,6 @@ while x == 0:
     else:
         print("lets go")
         x = 1
+for i in range(anzahl):
+    rolling()
+    
